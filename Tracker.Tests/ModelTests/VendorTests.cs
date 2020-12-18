7db28a0ea2fc6c836065ae3585e3f8f6a2e0d316
Tawnee Harris <tawneeh@icloud.com>
2020-12-18 10:52:18 -0800

    add test six good fail

--- Tracker.Tests/ModelTests/VendorTests.cs
+++ Tracker.Tests/ModelTests/VendorTests.cs
@@ -14,10 +14,24 @@ namespace Tracker.Tests
     }
 
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVnedor_Vendor()
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("Test Vendor", "Test Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string vendorName1 = "Test Vendor";
+      string description1 = "Test Description";
+      string vendorName2 = "Test Vendor 2";
+      string description2 = "Test Description 2";
+      Vendor newVendor1 = new Vendor(vendorName1, description1);
+      Vendor newVendor2 = new Vendor(vendorName2, description2);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      Assert.AreEqual(newList, result);
     }
 
   }
