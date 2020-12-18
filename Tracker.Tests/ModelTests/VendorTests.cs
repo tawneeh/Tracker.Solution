db46ec98cd16e11cbbd52bb5ec41619de3534e53
Tawnee Harris <tawneeh@icloud.com>
2020-12-18 08:38:04 -0800

    add VendorTests setup

---
+++ Tracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Tracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+
+  }
+}
