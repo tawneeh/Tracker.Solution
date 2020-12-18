fff6ffc389de0e0d36b485e86b098ee92ff96317
Tawnee Harris <tawneeh@icloud.com>
2020-12-18 13:42:55 -0800

    add Vendor routes

--- Tracker/Controllers/VendorsController.cs
+++ Tracker/Controllers/VendorsController.cs
@@ -27,5 +27,30 @@ namespace Tracker.Controllers
       Vendor newVendor = new Vendor(name, description);
       return RedirectToAction("Index");
     }
+
+    [HttpPost()]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderPrice, string orderDate)
+    {
+      Dictionary<string, object> tracker = new Dictionary<string, object>();
+      Vendor specificVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      specificVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = specificVendor.Orders;
+      tracker.Add("orders", vendorOrders);
+      tracker.Add("vendor", specificVendor);
+      return View("Show", tracker);
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> tracker = new Dictionary<string, object>();
+      Vendor specificVendor = Vendor.Find(id);
+      List<Order> vendorOrders = specificVendor.Orders;
+      tracker.Add("vendor", specificVendor);
+      tracker.Add("orders", vendorOrders);
+      return View(tracker);
+    }
+
   }
 }
