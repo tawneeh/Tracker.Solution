1568ec3df66be805b625682b40270975caa9f770
Tawnee Harris <tawneeh@icloud.com>
2020-12-18 11:58:12 -0800

    add order routes

--- Tracker/Controllers/OrdersController.cs
+++ Tracker/Controllers/OrdersController.cs
@@ -7,5 +7,23 @@ namespace Tracker.Controllers
   public class OrdersController : Controller
   {
 
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> track = new Dictionary<string, object>();
+      track.Add("order", order);
+      track.Add("vendor", vendor);
+      return View(track);
+    }
+
   }
 }
