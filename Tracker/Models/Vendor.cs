bffdcacec7c98614399db032301503e217ccd2b9
Tawnee Harris <tawneeh@icloud.com>
2020-12-18 11:21:08 -0800

    add tested methods

---
+++ Tracker/Models/Vendor.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace Tracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName, string description)
+    {
+      Name = vendorName;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> {};
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
