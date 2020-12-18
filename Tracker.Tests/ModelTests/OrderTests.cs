d97947bc40d6c92bf357aa06283da714dfda7dfe
Tawnee Harris <tawneeh@icloud.com>
2020-12-18 08:36:10 -0800

    add OrderTests setup

---
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Tracker.Models;
+using System; 
+
+namespace Tracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+
+  }
+}
